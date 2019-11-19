11d20442954ed8a22523622780cbefb7ad9712e0
Nicolas <info@inovea-conseil.com>
2019-11-19 15:35:04 +0100

    FIX duplicate supplier

--- class/soodispatch.class.php
+++ class/soodispatch.class.php
@@ -39,94 +39,96 @@ dol_include_once('soodispatch/lib/PHPExcel/IOFactory.php');
  *
  * @see CommonObject
  */
-class Soodispatch extends CommonObject {
+class Soodispatch extends CommonObject
+{
 
     /**
      * Constructor
      *
      * @param DoliDb $db Database handler
      */
-    public function __construct(DoliDB $db) {
+    public function __construct(DoliDB $db)
+    {
         $this->db = $db;
     }
-    
-    public function sheetprocess($file){
+
+    public function sheetprocess($file)
+    {
         global $user, $conf;
         if(file_exists($file)){
-             
-// Chargement du fichier Excel
-$objPHPExcel = PHPExcel_IOFactory::load($file);
- 
-/**
-* récupération de la première feuille du fichier Excel
-* @var PHPExcel_Worksheet $sheet
-*/
-$sheet = $objPHPExcel->getSheet(0);
-$suppliersf = array();
-echo '<table border="1">';
- 
-// On boucle sur les lignes
-$i = 0;
-foreach($sheet->getRowIterator() as $valline => $row) {
-    if($i == 0) {$i=1;continue;}
 
-    $namesup = $sheet->getCell("C".$valline)->getFormattedValue();
-    $supplier = $this->getSupplierByName($namesup);
-    $idsupplier = $supplier->rowid;
-    
-    if(!is_object($supplier)){ 
-        $supplier = $this->getSupplierByAlias($namesup);
-        $idsupplier = $supplier->rowid;
-        if(is_object($supplier)){
-            $upsup = new Societe($this->db);
-            $upsup->fetch($idsupplier);
-            $upsup->name_alias = $namesup;
-            $upsup->update($idsupplier,$user);
-        }
-    }
-    if(!is_object($supplier)){
-        $supplier = new Societe($this->db);
-        $supplier->nom = $namesup;
-        $supplier->name = $namesup;
-        $supplier->name_alias = '9'.$namesup;
-        $supplier->fournisseur = 1;
-        $supplier->client = 0;
-        $supplier->tva_assuj = 1;
-        $supplier->entity = $conf->global->entity;
-        $supplier->country_id = 1;
-        $idsupplier = $supplier->create($user);//tva_assuj
-    }
-    
-    if($supplier->tva_assuj==1) {$tva = 20;} else {$tva=0;}
-    $suppliersf[$idsupplier][] = array('designation' => "Prestation de service",
-                                    'price' => $sheet->getCell("D".$valline)->getFormattedValue(),
-                                    'tva' => $tva,
-                                    'date' => $sheet->getCell("A".$valline)->getFormattedValue());
-    
-    
-   echo '<tr>';
- 
-   // On boucle sur les cellule de la ligne
-   foreach ($row->getCellIterator() as $valcell => $cell) {
-      echo '<td>';
-      print_r($cell->getFormattedValue());
-      echo '</td>';
-   }
- 
-   echo '</tr>';
-   
-}
-echo '</table>';
+// Chargement du fichier Excel
+            $objPHPExcel = PHPExcel_IOFactory::load($file);
+
+            /**
+             * récupération de la première feuille du fichier Excel
+             * @var PHPExcel_Worksheet $sheet
+             */
+            $sheet = $objPHPExcel->getSheet(0);
+            $suppliersf = array();
+            echo '<table border="1">';
+
+// On boucle sur les lignes
+            $i = 0;
+            foreach($sheet->getRowIterator() as $valline => $row) {
+                if($i == 0) {$i=1;continue;}
+
+                $namesup = $sheet->getCell("C".$valline)->getFormattedValue();
+                $supplier = $this->getSupplierByName($namesup);
+                $idsupplier = $supplier->rowid;
+
+                if(!is_object($supplier)){
+                    $supplier = $this->getSupplierByAlias($namesup);
+                    $idsupplier = $supplier->rowid;
+                    if(is_object($supplier)){
+                        $supplier = new Societe($this->db);
+                        $supplier->fetch($idsupplier);
+                        $supplier->name_alias = $namesup;
+                        $supplier->update($idsupplier,$user);
+                    }
+                }
+                if(!is_object($supplier)){
+                    $supplier = new Societe($this->db);
+                    $supplier->nom = $namesup;
+                    $supplier->name = $namesup;
+                    $supplier->name_alias = $namesup;
+                    $supplier->fournisseur = 1;
+                    $supplier->client = 0;
+                    $supplier->tva_assuj = 1;
+                    $supplier->entity = $conf->global->entity;
+                    $supplier->country_id = 1;
+                    $idsupplier = $supplier->create($user);//tva_assuj
+                }
+
+                if($supplier->tva_assuj==1) {$tva = 20;} else {$tva=0;}
+                $suppliersf[$idsupplier][] = array('designation' => "Prestation de service",
+                    'price' => $sheet->getCell("D".$valline)->getFormattedValue(),
+                    'tva' => $tva,
+                    'date' => $sheet->getCell("A".$valline)->getFormattedValue());
+
+
+                echo '<tr>';
+
+                // On boucle sur les cellule de la ligne
+                foreach ($row->getCellIterator() as $valcell => $cell) {
+                    echo '<td>';
+                    print_r($cell->getFormattedValue());
+                    echo '</td>';
+                }
+
+                echo '</tr>';
+            }
+            echo '</table>';
         }
         foreach($suppliersf as $idsup => $lines){
 
             $invoice = new FactureFournisseur($this->db);
             $invoice->socid = $idsup;
             $invoice->date = dol_now();
-             $invoice->amount = 0;
-             $invoice->mode_reglement_id = 2;
+            $invoice->amount = 0;
+            $invoice->mode_reglement_id = 2;
             foreach($lines as $key => $line){
-           // echo "<pre>".print_r($line,1)."</pre>";
+                // echo "<pre>".print_r($line,1)."</pre>";
 
                 $invoice->lines[$key] = new SupplierInvoiceLine($this->db);
                 $invoice->lines[$key]->description = $line['designation'];
@@ -141,23 +143,21 @@ echo '</table>';
                 if($key == 0){
                     $d = explode('/', $line['date']);
                     $invoice->ref_supplier = $d[1].'/'.$d[2];
-                    
                 }
             }
-                                  //  echo "<pre>".print_r($invoice,1)."</pre>";
+            //  echo "<pre>".print_r($invoice,1)."</pre>";
 
             $invoice->create($user);
-
-            
         }
-        
+
         //echo "<pre>".print_r($suppliersf,1)."</pre>";
-        
+
         echo "<div style='color:green;font-size:25px'>La création des factures a été réalisée</div>";
     }
-    
-    
-    public function getSupplierByAlias($alias){
+
+
+    public function getSupplierByAlias($alias)
+    {
         $sql = 'SELECT * FROM '.MAIN_DB_PREFIX.'societe WHERE name_alias="'.$alias.'" LIMIT 1';
         $resql=$this->db->query($sql);
         if($this->db->num_rows($resql) > 0)
@@ -166,15 +166,16 @@ echo '</table>';
             $obj = "";
         return $obj;
     }
-    
-        public function getSupplierByName($alias){
+
+    public function getSupplierByName($alias)
+    {
         $sql = 'SELECT * FROM '.MAIN_DB_PREFIX.'societe WHERE name="'.$alias.'" LIMIT 1';
         $resql=$this->db->query($sql);
-            if($this->db->num_rows($resql) > 0)
-                $obj = $this->db->fetch_object($resql);
-            else
-                $obj = "";
+        if($this->db->num_rows($resql) > 0)
+            $obj = $this->db->fetch_object($resql);
+        else
+            $obj = "";
         return $obj;
     }
 }
- ?>
+
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-/* 
+/*
  * Copyright (C) 2017      Nicolas ZABOURI      <info@inovea-conseil.com>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -50,7 +50,7 @@ $confirm=GETPOST('confirm','alpha');
  * View
  */
 if (! $user->rights->soodispatch->read)
-	accessforbidden();
+    accessforbidden();
 
 $form = new Form($db);
 $formfile = new FormFile($db);
@@ -77,41 +77,38 @@ if(!empty($afile)){
 
     $file = $_FILES['userfile'];
     if(!file_exists($upload_dir)){
-        dol_mkdir($upload_dir);        
+        dol_mkdir($upload_dir);
     }
-    
-$dest_file = $upload_dir.dol_sanitizeFileName(dol_now().$file['name'][0]);
+
+    $dest_file = $upload_dir.dol_sanitizeFileName(dol_now().$file['name'][0]);
 
     $result = dol_move_uploaded_file($file['tmp_name'][0], $dest_file, 1, 0, 0);
-    
-    if(file_exists($dest_file)){
-        $excel = new Soodispatch($db);
-        $excel->sheetprocess($dest_file);
-        
-        setEventMessages("UploadedFile", null, 'mesgs');
-        
-    }else{
-        setEventMessages(null, $langs->trans('Problem'), 'errors');
-    }
+
+    /* if(file_exists($dest_file)){ */
+    $excel = new Soodispatch($db);
+    $excel->sheetprocess($dest_file);
+
+    /* }else{
+         setEventMessages(null, $langs->trans('Problem'), 'errors');
+     } */
 }
 if ($action == 'delete')
 {
-	$langs->load("companies");	// Need for string DeleteFile+ConfirmDeleteFiles
-	$ret = $form->form_confirm(
-			$_SERVER["PHP_SELF"] . '?urlfile=' . urlencode(GETPOST("urlfile")) . '&linkid=' . GETPOST('linkid', 'int') . (empty($param)?'':$param),
-			$langs->trans('DeleteFile'),
-			$langs->trans('ConfirmDeleteFile'),
-			'confirm_deletefile',
-			'',
-			0,
-			1
-	);
-	if ($ret == 'html') print '<br>';
+    $langs->load("companies");	// Need for string DeleteFile+ConfirmDeleteFiles
+    $ret = $form->form_confirm(
+        $_SERVER["PHP_SELF"] . '?urlfile=' . urlencode(GETPOST("urlfile")) . '&linkid=' . GETPOST('linkid', 'int') . (empty($param)?'':$param),
+        $langs->trans('DeleteFile'),
+        $langs->trans('ConfirmDeleteFile'),
+        'confirm_deletefile',
+        '',
+        0,
+        1
+    );
+    if ($ret == 'html') print '<br>';
 }
 if($action == "confirm_deletefile" && $confirm=="yes"){
-    	$file = $upload_dir . "/" . GETPOST('urlfile');	// Do not use urldecode here ($_GET and $_POST are already decoded by PHP).
-        	$ret=dol_delete_file($file);
-
+    $file = $upload_dir . "/" . GETPOST('urlfile');	// Do not use urldecode here ($_GET and $_POST are already decoded by PHP).
+    $ret=dol_delete_file($file);
 }
 $formfile=new FormFile($db);
 
@@ -120,9 +117,9 @@ $formfile=new FormFile($db);
 
 
 $modulepart = 'soodispatch';
-	$permission = $user->rights->fournisseur->facture->creer;
-	$permtoedit = $user->rights->fournisseur->facture->creer;
-	$param = '&id=' . 1;
+$permission = $user->rights->fournisseur->facture->creer;
+$permtoedit = $user->rights->fournisseur->facture->creer;
+$param = '&id=' . 1;
 // Show upload form (document and links)
 $formfile->form_attach_new_file(
     $_SERVER["PHP_SELF"],
@@ -132,9 +129,9 @@ $formfile->form_attach_new_file(
     $permission,
     $conf->browser->layout == 'phone' ? 40 : 60,
     '',
-	'',
-	1,
-	$savingdocmask,0,'soodispatch'
+    '',
+    1,
+    $savingdocmask,0,'soodispatch'
 );
 
 // List of document
@@ -157,4 +154,4 @@ $formfile->list_of_documents(
 
 print "<br>";
 
-?>
+
